afeb681dfef870c82177488fa8c0c718da1fd7d6
Sohaib Ali Syed <sohaibali01@gmail.com>
2020-03-06 15:00:48 +0500

    Update BFS.py

--- BFS.py
+++ BFS.py
@@ -1,18 +1,67 @@
-graph = {'A': set(['B','C', 'F']),
-         'B': set(['D','C', 'A']),
-         'C': set(['A','B', 'D', 'F']),
-         'D': set(['B','C','F','E']),
-         'E': set(['D','F']),
-         'F': set(['A','C','E'])}
 
-def dfs_paths(graph, start, goal):
-    stack = [(start, [start])]
-    while stack:
-        (vertex, path) = stack.pop()
-        for next in graph[vertex] - set(path):
-            if next == goal:
-                yield path + [next]
-            else:  
-                stack.append((next, path + [next]))
+def actionSequence(graph, initialState, goalState):
+    # returns a list of states starting from goal state moving upwards towards
+    # parents until root node is reached
+    solution=[goalState]
+    currentParent=graph[goalState].parent
+    while currentParent!=None:
+        solution.append(currentParent)
+        currentParent = graph[currentParent].parent
+    solution.reverse()
+    return solution
 
-print((list(dfs_paths(graph, 'A', 'E'))))
+class Node:
+    #state = state         # class variable shared by all instances
+    def __init__(self, state, parent, actions, totalCost):
+        self.state = state    # instance variable unique to each instance
+        self.parent = parent
+        self.actions = actions # we are not saving actions themselves,
+                               # only output states of those actions 
+        self.totalCost = totalCost
+
+def DFS():
+    initialState = 'D'
+    goalState = 'C'
+
+    # we think of a graph as a dictionary, items comprise of nodes, where
+    # each node has a key and a value. Key is simply the state of the node
+    # and value are actual attributes that node object
+
+    graph = {'A': Node('A', None, ['B', 'E', 'C'], None),
+             'B': Node('B', None, ['D', 'E', 'A'], None),
+             'C': Node('C', None, ['A', 'F', 'G'], None),
+             'D': Node('D', None, ['B', 'E'], None),
+             'E': Node('E', None, ['A', 'B','D'], None),
+             'F': Node('F', None, ['C'], None),
+             'G': Node('G', None, ['C'], None)}
+            
+    frontier = [initialState]
+    explored=[]
+
+    while len(frontier)!=0:
+        
+        currentNode = frontier.pop(len(frontier)-1)
+        
+        explored.append(currentNode)
+        #currentChildren=0
+        for child in graph[currentNode].actions:
+            if child not in frontier and child not in explored:
+                graph[child].parent=currentNode
+                if graph[child].state==goalState:
+                    #print(explored)
+                    return actionSequence(graph, initialState, goalState)
+                #currentChildren=currentChildren+1
+                frontier.append(child)
+                print(currentNode)
+                print(child)
+                print(frontier)
+            print()
+        print()
+        #if currentChildren==0:
+        #   del explored[len(explored)-1]
+    
+solution = DFS()
+print(solution)
+
+
+
